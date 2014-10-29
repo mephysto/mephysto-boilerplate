39d96f4fd62c81b85f4513d30250a36e993680d5
Maurice Melchers <maurice.melchers@gmail.com>
2014-10-28 17:49:19 -0700

    updated grunt & file structure
    - started using working folders
    - moved original build files into SRC
    - projects now compile into BUILD and PRODUCTION (minified depending on
    your needs)
    - only one watch needed, instead of several (use grunt watchall).
    Individual watches for images, scripts, and styles are still available.
    - added cssmin, htmlmin, notify, etc to the project

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,92 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+
 
   // Project configuration.
+
   grunt.initConfig({
+
     pkg: this.file.readJSON('package.json'),
 
-    vars: {
-      styles: 'css/',
+    // locations
+    cwd: 'src/',                // local folder, edit files here
+    build: 'build/',            // build folder, grunt will compile end result to here
+    production: 'production/',  // production folder, grunt will create minified assets here
+    
+    /* NOTIFY */
+    // throw up notification messages when a certain task has completed.
+    notify: {
+      watchJS: {
+        options: {
+          title: 'JavaScript finished building',
+          message: 'Java\s scripted'
+        }
+      },
+      watchCSS: {
+        options: {
+          title: 'Styles finished compiling',
+          message: 'Things are looking prettier'
+        }
+      },
+      watchIMG: {
+        options: {
+          title: 'Images finished optimizing',
+          message: 'Paintings painted'
+        }
+      },
+      buildcomplete: {
+        options: {
+          title: 'full build',
+          message: 'Carrier has arrived'
+        }
+      }
     },
 
-    // clean
-    clean: {
-      build: [""]
+    /* STYLES */
+    // process LESS into CSS
+    less: {
+      local: {
+        options: {
+          paths: ["<%= cwd %>css"],
+          ieCompat: true, // make work with IE8
+        },
+        files: [
+          {
+            src: "<%= cwd %>css/main.less",
+            dest: "<%= build %>css/styles.css",
+          }
+        ]
+      }
     },
 
+    // auto browserprefix for CSS
+    autoprefixer: {
+      local: {
+        options: {
+          // support for what: 
+          browsers: ['last 2 version', 'ie 8', 'ie 9']
+        },
+        files: [
+          {
+            src: '<%= build %>css/styles.css',
+            dest: '<%= build %>css/styles.pfx.css',
+          },
+        ]
+      }
+    },
+
+    // css minify
+    cssmin: {
+      production: {
+        files: [
+          {
+            src: '<%= build %>css/styles.pfx.css',
+            dest: '<%= production %>css/styles.min.css',
+          }
+        ]
+      }
+    },
+
+    /* SCRIPTS */
     // JS hint
     jshint: {
       options: {
@@ -21,136 +95,168 @@ module.exports = function(grunt) {
         eqnull: true,
         browser: true,
         globals: {
-          jQuery: true
-        }
-      },
-      // change this line to match your js files
-      all: ['js/main.js']
-    },
-
-    // process + minify LESS into CSS
-    less: {
-      development: {
-        options: {
-            paths: ["./css"]
+          jQuery: true,
         },
-        files: {
-          "css/styles.css": "css/main.less"
-        }
-      }
-    },
-    // auto browserprefix for CSS
-    autoprefixer: {
-      options: {
-        browsers: ['last 2 version', 'ie 8', 'ie 9']
       },
-      single_file: {
-        options: {
-           yuicompress: true
-        },
-        src: 'css/styles.css',
-        dest: 'css/build/styles.min.css'
-      },
+      all: ['<%= cwd %>js/main.js']
     },
-
     // minify JS
     uglify: {
-      build: {
+      local: {
+        options: {
+          mangle: false, // preserve variables
+          // sourceMap: true,
+          // sourceMapName: '<%= build %>js/sourcemap.map',
+          beautify: true,
+          // compress: { drop_console: true }
+        },
+        files: [
+          {
+            src: ['<%= cwd %>js/main.js'],
+            dest: '<%= build %>js/main.min.js',
+          }
+        ]
+      },
+      production: {
         options: {
           // mangle: false, // preserve variables
-
+          // sourceMap: true
+          // sourceMapName: '<%= build %>js/sourcemap.map'
+          // beautify: true
+          compress: { drop_console: true }
         },
-        src: ['js/main.js'],
-        dest: 'js/build/global.min.js'
+        files: [
+          {
+            src: ['<%= cwd %>js/main.js'],
+            dest: '<%= production %>js/main.min.js'
+          }
+        ]
       }
     },
-
-    // optimize images
-    imagemin: {
-      static: {
-        options: {
-          optimizationLevel: 3
-        },
-        files: [{
-          expand: true,
-          cwd: 'image/',
-          src: ['*.{png,jpg,gif}'],
-          dest: 'image/build/'
-        }]
-      },
-      dynamic: {
-        files: [{
-          expand: true,
-          cwd: 'image/',
-          src: ['*.{png,jpg,gif}'],
-          dest: 'image/build/'
-        }]
-      }
-    },
-
     // Concatenate all the minified files into one big file.
     concat: {
       options: {
         separator: ';',
       },
-      dist: {
-        src: ['js/*.js'],
-        dest: 'js/build/global.min.js'
+      build: {
+        files: [
+          {
+            src: ['<%= cwd %>js/vendor/modernizr-2.6.2.min.js', '<%= build %>js/main.min.js'],
+            dest: '<%= build %>js/global.min.js'
+          },
+          {
+            src: ['<%= cwd %>js/vendor/modernizr-2.6.2.min.js', '<%= cwd %>js/vendor/masonry.pkgd.min.js', '<%= build %>js/main.min.js'],
+            dest: '<%= build %>js/landing-scripts.min.js',
+          }
+        ]
       },
+      production: {
+        files: [
+          {
+            src: ['<%= cwd %>js/vendor/modernizr-2.6.2.min.js', '<%= production %>js/main.min.js'],
+            dest: '<%= production %>js/global.min.js',
+          },
+          {
+            src: ['<%= cwd %>js/vendor/modernizr-2.6.2.min.js', '<%= cwd %>js/vendor/masonry.pkgd.min.js', '<%= production %>js/main.min.js'],
+            dest: '<%= production %>js/landing-scripts.min.js',
+          }
+        ]
+      }
     },
 
+
+    /* IMAGES */
+    // optimize images
+    imagemin: {
+      localdev: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= cwd %>images/',
+          src: ['**/*.{png,jpg,gif,svg}'],
+          dest: '<%= build %>images/'
+        }]
+      },
+      dev: {
+        options: {
+          optimizationLevel: 3,
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= cwd %>images/',
+          src: ['**/*.{png,jpg,gif,svg}'],
+          dest: '<%= production %>images/'
+        }]
+      }
+    },
+
+    /* WATCH */
     // watch for changes in files
     watch: {
-      // watch for changes in CSS
+      // watch for changes in LESS files
       styles : {
-        files: ["./css/*.less"],
-        tasks: ['less', 'autoprefixer']
+        files: ["<%= cwd %>css/*.less"],
+        tasks: ['less', 'autoprefixer', 'cssmin', 'notify:watchCSS']
       },
       // watch for changes in script
       scripts : {
-        files: ['js/*.js'],
-        tasks: ['uglify']
+        files: ['<%= cwd %>js/*.js'],
+        tasks: ['jshint', 'uglify', 'concat', 'notify:watchJS']
       },
       // watch for updates in images
       images : {
-        files: ['images/src/**/*.{png,jpg,gif}'],
-        tasks: ['imagemin']
+        files: ['<%= cwd %>images/**/*.{png,jpg,gif,svg}'],
+        tasks: ['imagemin', 'notify:watchIMG']
       }
-    }
-
-
+    },
+    // clean up the build folders. be carefull with this. we don't want to lose our working files!
+    clean: {
+      local: ["<%= build %>"],
+      production: ["<%= production %>"],
+    },
+    // minify html, remove any comments, whitespaces, etc
+    htmlmin: {
+      local: {
+        cwd: "<%= cwd %>",
+        expand: true,
+        options: {
+        },
+        src: "**/*.html",
+        dest: "<%= build %>",
+      },
+      production: {
+        cwd: "<%= cwd %>",
+        expand: true,
+        options: {
+          removeComments: true,
+        },
+        src: "**/*.html",
+        dest: "<%= production %>",
+      }
+    },
+    // NYI
+    version: {
+      options: {
+        pkg: 'package.json'
+      },
+      build: {
+        options: {
+          prefix: '@version\\s*'
+        },
+        src: ['build/css/styles.min.css'],
+      },
+    },    
   });
 
-  grunt.registerTask('logo', '', function(){
-    grunt.log.writeln(['\n\n\n'+
-'                        .. .   .:IMMMMM~' + 
-'\n                      ..:7MMMMMMMMMMMM8.' + 
-'\n      . .  ..   :7MMMMMMM,,...8MMMMMMM' + 
-'\n        ~$MM,... IMMMMMM+8......MMMMMM .' + 
-'\n ZMMMMMMMMMMN......MMMMM$.......MMMMM.' + 
-'\nZ?MMMMMMMMMM....... MMM....7....MMMM+' + 
-'\n 8..MMMMMMMM........NM 8..M.....MMMM.' + 
-'\n..$  NMMMMMM.........77...:.D...MMM$ .' + 
-'\n   .=.7MMMMM..........=..M M8...MMM ..' + 
-'\n     I  MMMM....M.......7=DMZ...MM. ..' + 
-'\n    . .? ?M8....MN......M MMI...MM.' + 
-'\n        D..$....MM7.....:=MN+...D:.' + 
-'\n         . .....MMM....= MMD....O.' + 
-'\n        .  D....MMMM+..N.MM8.....  ' + 
-'\n         . .7...MMMMMMMMMMMO...O.       ' + 
-'\n            . +.IMMMMMMMMMM$..=. .      ' + 
-'\n            .. + .MMMMMMMMM7..+         ' + 
-'\n            .....I +MMMMMMMM,~ .        ' + 
-'\n                  8..MMMMMMM.$ .        ' + 
-'\n                .... .NMMMMMM           ' + 
-'\n                   . 8 .MMMM.           ' + 
-'\n                   .. ?. DMM  .         ' + 
-'\n                       .I.8:            ' + 
-'\n                      .  =   \n' + 
-'\n Running Maurice Melchers :: Boilerplate\n\n\n']);
+  // logo
+  grunt.registerTask('logo', '', function () {
+    grunt.log.writeln(['\n\n\n' + '                        .. .   .:IMMMMM~\n                      ..:7MMMMMMMMMMMM8.\n      . .  ..   :7MMMMMMM,,...8MMMMMMM\n        ~$MM,... IMMMMMM+8......MMMMMM .\n ZMMMMMMMMMMN......MMMMM$.......MMMMM.\nZ?MMMMMMMMMM....... MMM....7....MMMM+\n 8..MMMMMMMM........NM 8..M.....MMMM.\n..$  NMMMMMM.........77...:.D...MMM$ .\n   .=.7MMMMM..........=..M M8...MMM ..\n     I  MMMM....M.......7=DMZ...MM. ..\n    . .? ?M8....MN......M MMI...MM.\n        D..$....MM7.....:=MN+...D:.\n         . .....MMM....= MMD....O.\n        .  D....MMMM+..N.MM8.....  \n         . .7...MMMMMMMMMMMO...O.       \n            . +.IMMMMMMMMMM$..=. .      \n            .. + .MMMMMMMMM7..+         \n            .....I +MMMMMMMM,~ .        \n                  8..MMMMMMM.$ .        \n                .... .NMMMMMM           \n                   . 8 .MMMM.           \n                   .. ?. DMM  .         \n                       .I.8:            \n                      .  =   \n\n Running Maurice Melchers :: Boilerplate\n\n\n']);
   });
 
   // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-less');
@@ -158,18 +264,21 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-
-  // Default task(s).
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-version');
+  grunt.loadNpmTasks('grunt-notify');
+  // grunt.loadNpmTasks('grunt-svgmin');
 
   /* ## Build site */
-  grunt.registerTask('default', ['logo', 'less', 'autoprefixer','jshint','uglify', 'imagemin']);
-
+  grunt.registerTask('default', ['logo', 'clean', 'htmlmin', 'less', 'autoprefixer', 'cssmin', 'jshint', 'uglify', 'concat', 'imagemin']);
   // start watching for changes in LESS
-  grunt.registerTask('watchstyles', ['logo','less', 'autoprefixer', 'watch:styles']);
+  grunt.registerTask('watchstyles', ['logo', 'less', 'autoprefixer', 'cssmin', 'watch:styles']);
   // start watching for changes in JS
-  grunt.registerTask('watchscripts', ['logo','jshint','uglify', 'watch:scripts']);
-  // start watching for changes in image folder
-  grunt.registerTask('watchimages', ['logo','imagemin', 'watch:images']);
+  grunt.registerTask('watchscripts', ['logo', 'jshint', 'uglify', 'concat', 'watch:scripts']);
 
+  // start watching for changes in image folder
+  grunt.registerTask('watchimages', ['logo', 'imagemin', 'watch:images']);
+  // run all tasks + watch
+  grunt.registerTask('watchall', ['default', 'watch']);
 };
